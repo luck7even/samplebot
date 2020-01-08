5d039842af67b6a4e934fade2c7506a94e7fde54
heungki.kim <heungki.kim@navercorp.com>
2020-01-08 11:21:47 +0900

    init

---
+++ conf/config.py
@@ -0,0 +1,30 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+# default language ['en'|'ko'|'ja']
+DEFAULT_LANG = 'ko'
+
+# TimeZone
+TZone = "Asia/Seoul"
+
+# account
+ADMIN_ACCOUNT = "admin@bt.lwba.kr"
+DOMAIN_ID = 137165
+
+# api
+API_ID = "kr1gSndKKVKKC"
+SERVER_CONSUMER_KEY = "mPzw7vtNZ8cxZVNMajIR"
+
+SERVER_ID = "c677860c39ad46e2af5c8734ddb5b476"
+SECRET_KEY_NAME = "private_20200108095218.key"
+
+# LOCAL ADDRESS
+LOCAL_ADDRESS = "https://samplebot20200108.herokuapp.com/"
+
+# DB config
+DB_HOST = "ec2-174-129-33-25.compute-1.amazonaws.com"
+DB_NAME = "d9perbc6id8hdm"
+DB_USER = "xoppmgzfemcgya"
+DB_PORT = "5432"
+DB_PASSWORD = "e389526a09c10e5218ce1df4763e732f0d358ba16cde4747c74ff501b0dbcfb4"
